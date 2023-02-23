f129eae60f9e665ff974d67c7d64f72958485e73
DanielY1108 <scarlet040@gmail.com>
2023-02-23 15:20:03 +0900

    Initial Commit

---
+++ MovieTrending/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieTrending
+//
+//  Created by JINSEOK on 2023/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
